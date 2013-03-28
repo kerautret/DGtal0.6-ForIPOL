f7c43414ef87dc7392e928207d2c4bbd2cc9f1f8
Kerautret <kerautre@loria.fr>
2013-03-28 13:46:32 +0100

    Add imagene args

--- cmake/CheckDGtalDependencies.cmake
+++ cmake/CheckDGtalDependencies.cmake
@@ -13,8 +13,16 @@ set(Boost_FOUND TRUE)
 message(STATUS "Boost and boost_program_options found.")
 
 
+set(ImaGeneArg_USE_STATIC_LIBS   ON)
+set(ImaGeneArg_USE_MULTITHREADED ON)
+set(ImaGeneArg_USE_STATIC_RUNTIME OFF)
+set(ImaGeneArg_FOUND TRUE)
+message(STATUS "ImaGeneArg found.")
+
 SET(Boost_INCLUDE_DIRS ${CMAKE_CURRENT_SOURCE_DIR}/src/boost-1.48)
+SET(ImaGeneArg_INCLUDE_DIRS ${CMAKE_CURRENT_SOURCE_DIR}/src/ImaGene)
 include_directories( ${Boost_INCLUDE_DIRS} )
+include_directories( ${ImaGeneArg_INCLUDE_DIRS} )
 SET(DGtalLibInc ${Boost_INCLUDE_DIRS} ${ImaGeneArg})
 
 
